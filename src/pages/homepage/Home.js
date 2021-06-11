d90cc2c32408827477a287ee0ac9b80f0747a6d8
sibghat <53030222+sibghat0@users.noreply.github.com>
2021-06-11 14:53:18 +0530

    pull

--- src/pages/homepage/Home.js
+++ src/pages/homepage/Home.js
@@ -1,33 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import "./Home.css";
-import img from "../../assets/career-path-development.jpg"
-import img1 from "../../assets/career-woman-using-her-smartphone-260nw-1186179382.jpg"
+import img from "../../assets/career-path-development.jpg";
+import img1 from "../../assets/career-woman-using-her-smartphone-260nw-1186179382.jpg";
+import Navbar from "../../components/navbar/navbar";
 
- class Home extends Component {
-    render() {
-        return (
-            
-            <div className="container">
-            <div className="navbar">
-                      <ul className="links">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Services</a></li>  
-    
-                </ul>
-            </div>
-            <div className="imgCont">
-            <img src={img}></img>
-            </div>
-            <div className="info">
-            <img src={img1} />
-            <div className="text">
+class Home extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Navbar />
+        <div className="imgCont">
+          <img src={img}></img>
+        </div>
+        <div className="info">
+          <img src={img1} />
+          <div className="text">
             <h4>Heading 1</h4>
-            <p>ffhssbfiugfdhsaasdsagdsaodyasdsadhasdsadsadfasdasdfadas fhgasdasdhsasfvdosadhasdvasdsgfdgfdgfdgfgfgdgfggfdhfdfgfggsdgsgsd asfhasidbsadgaivdiaFFFFFFFFDAiryfdQUYSFQytsdfuQYCSnhfbfgffgww</p>
-            </div>
-            </div>
-            </div>
-        )
-    }
+            <p>
+              ffhssbfiugfdhsaasdsagdsaodyasdsadhasdsadsadfasdasdfadas
+              fhgasdasdhsasfvdosadhasdvasdsgfdgfdgfdgfgfgdgfggfdhfdfgfggsdgsgsd
+              asfhasidbsadgaivdiaFFFFFFFFDAiryfdQUYSFQytsdfuQYCSnhfbfgffgww
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default Home;
