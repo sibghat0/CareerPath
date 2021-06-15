c7b33c0ca65f879065047a08914dbe6a66eef61b
sibghat <53030222+sibghat0@users.noreply.github.com>
2021-06-15 23:56:25 +0530

    sorry

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,17 +1,27 @@
-import React, { Component } from 'react'
-import "./navbar.css"
+import React, { Component } from "react";
+import "./navbar.css";
 
 export default class Navbar extends Component {
-    render() {
-        return (
-             <div className="navbar">
-                      <ul className="links">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Services</a></li>  
-    
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="navbar">
+        <div className="title">
+          <h4>Title</h4>
+        </div>
+        <div className="other">
+          <ul className="links">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
